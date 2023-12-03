3575546d889bd3846074367e785905236ddd0cae
Adheesh Nayak <92870896+Adheesh2@users.noreply.github.com>
2023-12-03 17:28:12 +0530

    Update main.py with Updated count_batteries_by_health Function
    Updated the Function count_batteries_by_health

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 
 def count_batteries_by_health(present_capacities):
-  return {
+  counts = {
     "healthy": 0,
     "exchange": 0,
     "failed": 0
   }
+  rated_capacity = 120  # Rated capacity of a new battery in Ah taken according to Example
+  for capacity in present_capacities:
+      SoH = (capacity / rated_capacity) * 100
+      
+      if 100 >= SoH >= 80:
+          counts["healthy"] += 1
+      elif 62 <= SoH <= 80:
+          counts["exchange"] += 1
+      else:
+          counts["failed"] += 1
+  
+  return counts
 
 
 def test_bucketing_by_health():
